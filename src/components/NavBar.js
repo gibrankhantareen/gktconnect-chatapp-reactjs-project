5bfcde1f5799a72ba1e32cec1057b6bfecb0aaa1
Gibran Khan Tareen <gibrantareen@gmail.com>
2023-02-22 00:43:54 +0530

    deploying on github.io

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,7 @@
 import { auth } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
-import React, { useState } from "react";
+import { useState } from "react";
 import GoogleSignin from "../img/btn_google_signin_dark_pressed_web.png";
 
 const NavBar = () => {
