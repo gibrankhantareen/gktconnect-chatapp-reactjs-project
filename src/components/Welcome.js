7da785da3d4c4290786f1bd0fd0d1f10f87c49d6
Gibran Khan Tareen <90929264+gibrankhantareen@users.noreply.github.com>
2023-04-27 11:31:39 +0530

    Minor fix

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -11,7 +11,7 @@ const Welcome = () => {
 
   return (
     <main className="welcome">
-      <h2>Welcome to The GKTConnect - Chat App</h2>
+      <h2>Welcome to My GKTConnect - Chat App</h2>
       <img src="/logo512.png" alt="ReactJs logo" width={50} height={50} />
       <p>Sign in with Google to chat with your Friends</p>
       <button className="sign-in">
